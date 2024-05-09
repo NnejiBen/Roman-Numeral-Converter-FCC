d649f0bf1cc35ebddffcdbda09013564ea6054f7
Benjamin Nneji <nnejiben@gmail.com>
2024-05-09 10:13:19 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Grab number input
+const number = document.getElementById("number");
+
+//Grab convert button
+const convertBtn = document.getElementById("convert-btn");
+
+//Grab output element
+const output = document.getElementById("output");
+
